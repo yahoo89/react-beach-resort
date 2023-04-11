c74f776d0e8264dbe627b54768ae0e99b83a8928
Tigran <tigran2589@gmail.com>
2023-04-11 20:21:10 +0300

    hotel beach resort

---
+++ src/components/RoomsContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { withRoomConsumer } from '../context'
+import Loading from './Loading'
+import RoomsFilter from './RoomsFilter'
+import RoomsList from './RoomsList'
+
+function RoomsContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context
+  if (loading) {
+    return <Loading />
+  }
+  return (
+    <>
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
+    </>
+  )
+}
+export default withRoomConsumer(RoomsContainer)
---
+++ src/pages/Rooms.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import Banner from '../components/Banner'
+import Hero from '../components/Hero'
+import RoomsContainer from '../components/RoomsContainer'
+
+const Rooms = () => {
+  return (
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer />
+    </>
+  )
+}
+
+export default Rooms
