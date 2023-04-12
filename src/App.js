16cec7ba993f0d59f518710bb5f07b4c587fd488
Tigran <tigran2589@gmail.com>
2023-04-12 11:57:28 +0300

    fix scroll to top

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 import Navbar from './components/Navbar'
+import ScrollToTop from './fixes/ScrollToTop'
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
